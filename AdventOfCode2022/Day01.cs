67d2c93a3d3521d2447c79b6491030ffb295de44
Rick Gray <rickdgray@outlook.com>
2022-12-02 17:41:33 -0600

    day 1

---
+++ AdventOfCode2022/Day01.cs
@@ -0,0 +1,57 @@
+﻿namespace AdventOfCode2022
+{
+    public class Day01
+    {
+        public static long Part1(List<string> data)
+        {
+            var index = 0;
+            var elves = new List<long> { 0 };
+
+            foreach (var item in data)
+            {
+                if (string.IsNullOrWhiteSpace(item))
+                {
+                    index++;
+                    elves.Add(0);
+                    continue;
+                }
+
+                elves[index] += long.Parse(item);
+            }
+
+            return elves.Max();
+        }
+
+        public static long Part2(List<string> data)
+        {
+            var index = 0;
+            var elves = new List<long> { 0 };
+
+            foreach (var item in data)
+            {
+                if (string.IsNullOrWhiteSpace(item))
+                {
+                    index++;
+                    elves.Add(0);
+                    continue;
+                }
+
+                elves[index] += long.Parse(item);
+            }
+
+            var heap = new PriorityQueue<long, long>();
+
+            foreach (var elf in elves)
+            {
+                heap.Enqueue(elf, elf);
+
+                if (heap.Count > 3)
+                {
+                    heap.Dequeue();
+                }
+            }
+
+            return heap.UnorderedItems.Sum(e => e.Element);
+        }
+    }
+}
---
+++ AdventOfCode2022/Program.cs
@@ -0,0 +1,16 @@
+﻿using AdventOfCode2022;
+
+var workingDirectory = new DirectoryInfo(Directory.GetCurrentDirectory());
+var path = Path.Combine(workingDirectory?.Parent?.Parent?.Parent?.FullName ?? throw new DirectoryNotFoundException(), "Data", "day01.txt");
+using var fileStream = File.OpenRead(path);
+using var streamReader = new StreamReader(fileStream);
+
+var data = new List<string>();
+string? line;
+while ((line = streamReader.ReadLine()) != null)
+{
+    data.Add(line);
+}
+
+//currently working on:
+Console.WriteLine(Day01.Part2(data));
